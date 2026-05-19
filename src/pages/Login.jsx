004b65c0dd251fb02323ef8edf7c176ca69fce32
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2026-05-19 12:58:52 +0300

    handle register & login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,83 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  return <div>loooginnnnn</div>;
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required("Email is required")
+        .email("Invalid email format"),
+      password: Yup.string().required("Password is required"),
+    }),
+
+    onSubmit: async (values) => {
+      try {
+        const { data } = await axios.post(
+          "https://ecommerce.routemisr.com/api/v1/auth/signin",
+          values,
+        );
+
+        console.log("Login success:", data);
+
+        // save token
+        localStorage.setItem("token", data.token);
+
+        // go to home
+        navigate("/");
+      } catch (error) {
+        console.error("Login error:", error);
+
+        const msg = error.response?.data?.message || "Login failed";
+        alert(msg);
+      }
+    },
+  });
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.email && formik.errors.email && (
+          <p style={{ color: "red" }}>{formik.errors.email}</p>
+        )}
+
+        <br />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.password && formik.errors.password && (
+          <p style={{ color: "red" }}>{formik.errors.password}</p>
+        )}
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,40 +1,65 @@
 import { useFormik } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
-
+import axios from "axios";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
 function Register() {
   let navigate = useNavigate();
+  let [error, setError] = useState("");
+  let [loading, setLoading] = useState(false);
   let formik = useFormik({
     initialValues: {
-      username: "",
+      name: "",
       email: "",
       password: "",
+      rePassword: "",
+      phone: "",
     },
     onSubmit: handlesubmit,
     validationSchema: Yup.object({
-      username: Yup.string().required("Name is required"),
+      name: Yup.string().required("Name is required"),
       email: Yup.string().required("Email is required"),
       password: Yup.string().required("Password is required"),
+      rePassword: Yup.string()
+        .required("Re Password is required")
+        .oneOf([Yup.ref("password")], "Passwords do not match"),
+      phone: Yup.string()
+        .required("Phone is required")
+        .matches(/^01[0-2]{1}[0-9]{8}$/, "Invalid phone number"),
     }),
   });
 
-  function handlesubmit(values) {
-    console.log(values);
-    navigate("/Login");
+  async function handlesubmit(values) {
+    try {
+      setLoading(true);
+      let { data } = await axios.post(
+        "https://ecommerce.routemisr.com/api/v1/auth/signup",
+        values,
+      );
+      console.log(data);
+      navigate("/login");
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      console.log(error.response.data.message);
+      setLoading(false);
+    }
   }
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
+        {error && <p className="text-danger">{error}</p>}
         <label>Name</label>
         <input
           type="text"
-          name="username"
-          value={formik.values.username}
+          name="name"
+          value={formik.values.name}
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
         />
-        {formik.errors.username && formik.touched.username && (
-          <p>{formik.errors.username}</p>
+        {formik.errors.name && formik.touched.name && (
+          <p>{formik.errors.name}</p>
         )}
         <br />
         <label>Email</label>
@@ -48,6 +73,29 @@ function Register() {
           <p>{formik.errors.email}</p>
         )}
         <br />
+        <label>Phone</label>
+        <input
+          type="text"
+          name="phone"
+          value={formik.values.phone}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.errors.phone && formik.touched.phone && (
+          <p>{formik.errors.phone}</p>
+        )}
+        <br />
+        <label>Re Password</label>
+        <input
+          type="password"
+          name="rePassword"
+          value={formik.values.rePassword}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.rePassword && formik.touched.rePassword && (
+          <p>{formik.errors.rePassword}</p>
+        )}
+        <br />
         <label>Password</label>
         <input
           type="password"
@@ -59,7 +107,14 @@ function Register() {
           <p>{formik.errors.password}</p>
         )}
         <br />
-        <button type="submit">Register</button>
+        <button type="submit">
+          {loading ? (
+            <Loader2 className="animate-spin" size={20} />
+          ) : (
+            "Register"
+          )}
+        </button>
+        <Link to="/login">Login</Link>
       </form>
       <Link to="/">Home</Link>
     </div>
